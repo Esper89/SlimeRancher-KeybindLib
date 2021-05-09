b314e36141eaf426aab9a871d2307175d26ce889
Esper Thomson <esperenzathomson@gmail.com>
2021-05-09 00:51:55 -0400

    Improvements to TestMod documentation.

--- src/TestMod/Main.cs
+++ src/TestMod/Main.cs
@@ -15,6 +15,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with KeybindLib.  If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using SRML;
 
 namespace TestMod
@@ -30,7 +31,10 @@ namespace TestMod
 
         public override void Load()
         {
-            // TODO Action access example.
+            if (Keybinds.Test.Action.IsPressed) // `Keybind.Action` can only be accessed after the PreLoad step.
+            {
+                Console.WriteLine("Hello World!");
+            }
 
             // ...
         }
---
+++ src/TestMod/README.md
@@ -0,0 +1,7 @@
+﻿# TestMod
+
+This mod serves as both an example implementation and a test mod for KeybindLib. You can also refer to the GitHub wiki (`.github/wiki`) or the main README for more information.
+
+Check out `Main.cs` for the mod's entry point, `Keybinds.cs` for the actual example, or `modinfo.json`.
+
+If you have any questions, feel free to message me on Discord (`Esper#8989`) or matrix (`@esper89:matrix.org`).
